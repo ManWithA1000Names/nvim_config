67d4cef3a3e060e162d20103040795fcdd9b40bb
ManWithA1000Names <loena.phivos2010@gmail.com>
2023-04-19 15:59:11 +0300

    fix(nvim-tree): patch nvim-tree to work without warnings

--- lua/quri/nvim-tree.lua
+++ lua/quri/nvim-tree.lua
@@ -4,59 +4,14 @@ if not ok then
 	return
 end
 
+local on_attach = require("quri.nvim-tree-custom-on-attach")
+
 tree.setup({
+	on_attach = on_attach,
 	sync_root_with_cwd = true,
 	respect_buf_cwd = true,
 	update_focused_file = {
 		enable = true,
 		update_root = true,
 	},
-	view = {
-		mappings = {
-			list = {
-				{ key = { "l", "<CR>", "o" }, action = "edit", mode = "n" },
-				{ key = "h", action = "close_node" },
-			},
-		},
-	},
 })
-
---[[ tree.setup({ ]]
---[[ on_attach = function(bufnr) ]]
---[[ 	vim.schedule(function() ]]
---[[ 		vim.api.nvim_set_option_value("cursorline", true, { buf = bufnr }) ]]
---[[ 	end) ]]
---[[ 	vim.keymap.set("n", "l", function() ]]
---[[ 		api.node.open.edit() ]]
---[[ 	end, { noremap = true, buffer = bufnr }) ]]
---[[ 	vim.keymap.set("n", "h", function() ]]
---[[ 		api.node.navigate.parent_close() ]]
---[[ 	end, { noremap = true, buffer = bufnr }) ]]
---[[ end, ]]
---[[ hijack_netrw = true, ]]
---[[ open_on_setup = true, ]]
---[[ open_on_setup_file = false, ]]
---[[ update_focused_file = { ]]
---[[ 	enable = true, ]]
---[[ 	update_root = true, ]]
---[[ }, ]]
---[[ hijack_directories = { ]]
---[[ 	enable = true, ]]
---[[ 	auto_open = true, ]]
---[[ }, ]]
---[[ diagnostics = { ]]
---[[ 	enable = true, ]]
---[[ 	show_on_dirs = true, ]]
---[[ 	icons = { ]]
---[[ 		hint = "", ]]
---[[ 		info = "", ]]
---[[ 		warning = "", ]]
---[[ 		error = "", ]]
---[[ 	}, ]]
---[[ }, ]]
---[[ renderer = { ]]
---[[ 	indent_markers = { ]]
---[[ 		enable = true, ]]
---[[ 	}, ]]
---[[ }, ]]
---[[ }) ]]
